e9137f605a5c2221dbd4709ff01ce5e6f1ba38d9
mauriziomonti <mauriziomonti@hotmail.com>
2022-06-21 14:32:35 +0100

    embedded video

--- soc-dashboard/src/Breathe/index.js
+++ soc-dashboard/src/Breathe/index.js
@@ -2,6 +2,9 @@ function Breathe(props) {
   return (
     <div id="breathe-container" className="containers">
       {props.children}
+      <iframe width="90%"
+      src="https://www.youtube.com/embed/BHY0FxzoKZE">
+      </iframe>
     </div>
   );
 }
