5c5ed33e738bed7557a80b8c54f9afc0cecc768a
jimlikesjazz <jim@jimwynn.co.uk>
2022-06-22 18:45:03 +0100

    fixed fetch

--- soc-dashboard/src/Dropdown/index.js
+++ soc-dashboard/src/Dropdown/index.js
@@ -4,22 +4,22 @@ function Dropdown({ onChange }) {
   return (
     <React.Fragment>
       <select name="weeks" id="week" onChange={onChange}>
-        <option value="week1">Week 1</option>
-        <option value="week2">Week 2</option>
-        <option value="week3">Week 3</option>
-        <option value="week4">Week 4</option>
-        <option value="week5">Week 5</option>
-        <option value="week6">Week 6</option>
-        <option value="week7">Week 7</option>
-        <option value="week8">Week 8</option>
-        <option value="week9">Week 9</option>
-        <option value="week10">Week 10</option>
-        <option value="week11">Week 11</option>
-        <option value="week12">Week 12</option>
-        <option value="week13">Week 13</option>
-        <option value="week14">Week 14</option>
-        <option value="week15">Week 15</option>
-        <option value="week16">Week 16</option>
+        <option value="Week1">Week 1</option>
+        <option value="Week2">Week 2</option>
+        <option value="Week3">Week 3</option>
+        <option value="Week4">Week 4</option>
+        <option value="Week5">Week 5</option>
+        <option value="Week6">Week 6</option>
+        <option value="Week7">Week 7</option>
+        <option value="Week8">Week 8</option>
+        <option value="Week9">Week 9</option>
+        <option value="Week10">Week 10</option>
+        <option value="Week11">Week 11</option>
+        <option value="Week12">Week 12</option>
+        <option value="Week13">Week 13</option>
+        <option value="Week14">Week 14</option>
+        <option value="Week15">Week 15</option>
+        <option value="Week16">Week 16</option>
       </select>
     </React.Fragment>
   );
